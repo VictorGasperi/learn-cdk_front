241087bd7c5a5e2f0c241542bcc2bcfab26d72d0
Victor Gasperi <victorgasperi@gmail.com>
2025-04-30 18:50:00 -0300

    Add the config for cloud deployment

--- iac/bin/learn-cdk_front.ts
+++ iac/bin/learn-cdk_front.ts
@@ -1,16 +1,25 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { LearnCdkFrontStack } from '../lib/learn-cdk_front-stack';
+import * as dotenv from 'dotenv'
+import * as cdk from "aws-cdk-lib";
+import { LearnCdkFrontStack } from "../lib/learn-cdk_front-stack";
+import path = require("path");
+
+dotenv.config( {path: '../../.env'} )
 
 const app = new cdk.App();
-new LearnCdkFrontStack(app, 'LearnCdkFrontStack', {
+new LearnCdkFrontStack(app, "LearnCdkFrontStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+  stage: process.env.STAGE,
+  path: path.join(__dirname, '..', '..', 'dist')
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
--- iac/lib/learn-cdk_front-stack.ts
+++ iac/lib/learn-cdk_front-stack.ts
@@ -1,16 +1,71 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { BlockPublicAccess } from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+export interface LearnCdkFrontProps extends cdk.StackProps {
+  stage?: string;
+  path: string;
+}
+
 export class LearnCdkFrontStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: LearnCdkFrontProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const { stage, path } = props;
+    console.log(stage);
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LearnCdkFrontQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const staticWebsiteBucket = new cdk.aws_s3.Bucket(
+      this,
+      `react-app-bucket-${stage}`,
+      {
+        bucketName: `learn-cdk-react-app-${stage}`,
+        websiteIndexDocument: "index.html",
+        websiteErrorDocument: "index.html",
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+        blockPublicAccess: new cdk.aws_s3.BlockPublicAccess({
+          blockPublicAcls: false,
+          ignorePublicAcls: false,
+          blockPublicPolicy: false,
+          restrictPublicBuckets: false
+        }),
+        publicReadAccess: false
+      }
+    );
+
+    staticWebsiteBucket.addToResourcePolicy(
+      // torna o bucket publico
+      new cdk.aws_iam.PolicyStatement({
+        sid: "learn-cdk-s3BucketPublicRead",
+        effect: cdk.aws_iam.Effect.ALLOW,
+        actions: ["s3:GetObject"],
+        principals: [new cdk.aws_iam.AnyPrincipal()],
+        resources: [`${staticWebsiteBucket.bucketArn}/*`],
+      })
+    );
+
+    const distribution = new cdk.aws_cloudfront.Distribution(
+      this,
+      `learn-cdk-react-app-disto-${stage}`,
+      {
+        priceClass: cdk.aws_cloudfront.PriceClass.PRICE_CLASS_100,
+        defaultBehavior: {
+          origin: new cdk.aws_cloudfront_origins.S3StaticWebsiteOrigin(staticWebsiteBucket)
+        },
+        comment: `CloudFront Distro for learn-cdk-react-app`
+      }
+    );
+
+    new cdk.aws_s3_deployment.BucketDeployment(this,
+      `learn-cdk-react-app-deployment-${stage}`,
+      {
+        destinationBucket: staticWebsiteBucket,
+        sources: [ cdk.aws_s3_deployment.Source.asset(path) ],
+        cacheControl: [
+          cdk.aws_s3_deployment.CacheControl.maxAge(cdk.Duration.days(1))
+        ],
+        distribution
+      }
+    )
   }
 }
